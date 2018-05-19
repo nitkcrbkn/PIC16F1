719dcf886e44255f89dd3080687b9d89cfa032c7
GarAcT <shot.kit.garact@gmail.com>
2018-05-19 12:44:19 +0900

    [upgrade] ECCPのライブラリを更新
    現在のMDで使われているものは完全に作り変えられたものだったようなので置換

--- inc/ECCP.h
+++ inc/ECCP.h
@@ -1,34 +1,36 @@
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//木更津高専ロボット研究同好会　
-//フルブリッジフルN_MOSFETモータードライバー用関数
-//Use device microchip PIC16F1938
-//MPLAB X IDE(ver.2.30)
-//HI-TECH C Compiler for PIC10/12/16 MCUs Version 9.80 in Lite mode  
-//Last updata 2015/5/12/
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/* 
+ * File:   ECCP.h
+ * Author: seimi
+ *
+ * Created on 2016/06/28, 17:03
+ */
 
 #ifndef ECCP_H
 #define ECCP_H
 
 #include <stdint.h>
 
-#define P1A LATC2	//!< ECCPの出力ポート
+#define P1A LATC2	//ECCPの出力ポート
 #define P1B LATB2
 #define P1C LATB1
 #define P1D LATB4
 
-#define LED1 LATA3	//状態表示用LEDポート//
+#define LED1 LATA3	//状態表示用LEDポート
 #define LED2 LATA4
-/**
-ECCPを初期化し、PWMを使えるようにします
-*/
-void pwm_init(void);
 
-/**
-モーターの方向、回転数を制御する関数。
-\param duty 0~800までの整数
-\param dire 0->フリー動作　1->正転　2->逆転　3->ブレーキ
-*/
-void motor_ctrl(uint16_t duty, unsigned char dire);
+#define MAX_DUTY 999//デューティ値の上限は1023だが、回転方向変更時、危険なため制限
 
+typedef enum{
+  FREE_MODE,//フリー動作
+  FORWARD_MODE,//正転
+  BACK_MODE,//逆転
+  BRAKE_MODE,//ブレーキ
+} drive_mode;
+
+void PWMInit(void);
+//機能: PWMを使うための初期設定
+
+void PWMSet(uint16_t duty, drive_mode MODE);
+//機能: モーターの方向、回転数を制御する関数。
+//引数: duty...デューティ値(0~999) dire 動作(0->フリー動作　1->正転　2->逆転　3->ブレーキ)
 #endif	/* ECCP_H */
